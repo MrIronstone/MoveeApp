5505c099c4efcf3da3e07ee1abc04c6e77c4dcab
Hüsamettin Demirtaş <husamettin.demirtas@adesso.com.tr>
2022-11-26 00:46:34 +0300

    [MA-003] Tab Bar And TV Series Design
    * implemented tab bar and tv series views
    * implemented new methods for tv series requests to the network engine
    * fixed cardview

---
+++ MoveeApp/Views/TvSeriesView/TvSeriesView.swift
@@ -0,0 +1,20 @@
+//
+//  TvSeriesView.swift
+//  MoveeApp
+//
+//  Created by Demirtas, Husamettin on 24.11.2022.
+//
+
+import SwiftUI
+
+struct TvSeriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TvSeriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TvSeriesView()
+    }
+}
---
+++ MoveeApp/Views/TvSeriesView/TvSeriesViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TvSeriesViewModel.swift
+//  MoveeApp
+//
+//  Created by Demirtas, Husamettin on 24.11.2022.
+//
+
+import SwiftUI
+
+struct TvSeriesViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TvSeriesViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        TvSeriesViewModel()
+    }
+}
